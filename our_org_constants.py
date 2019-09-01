fb796b13c0a6d8ca982fb730d1c2537a193a05bc
Philip Chen <chenphilip@users.noreply.github.com>
2019-08-31 19:19:33 -0700

    Initial Commit

--- our_org_constants.py
+++ our_org_constants.py
@@ -6,23 +6,25 @@
 
 __author__ = "Philip Chen"
 __license__ = "https://opensource.org/licenses/GPL-3.0 GPL-3.0 License"
-__date__ = "2019.01.24"
-__version__ = "1.0.1"
+__date__ = "2019.08.31"
+__version__ = "1.0.2"
 __status__ = "Tested on Python 3.6, ArcGIS API 1.5.2"
 
 OUR_AGO_URL = "https://bcitgis.maps.arcgis.com/"
 
-OUR_ARC_PRO_ENTITLEMENTS = [	'3DAnalystN',
-				'airportsN',
-				'dataInteropN',
-				'dataReviewerN',
-				'defenseN',
-				'desktopAdvN',
-				'geostatAnalystN',
-				'imageAnalystN',
-				'maritimeN',
-				'networkAnalystN',
-				'productionMapN',
-				'publisherN',
-				'spatialAnalystN',
-				'workflowMgrN']
+OUR_ARC_PRO_ENTITLEMENTS = ['3DAnalystN',
+                            'airportsN',
+                            'dataInteropN',
+                            'dataReviewerN',
+                            'defenseN',
+                            'desktopAdvN',
+                            'geostatAnalystN',
+                            'imageAnalystN',
+                            'maritimeN',
+                            'networkAnalystN',
+                            'productionMapN',
+                            'publisherN',
+                            'spatialAnalystN',
+                            'workflowMgrN']
+
+MAX_NUMBER_OF_RETURNS = 200
